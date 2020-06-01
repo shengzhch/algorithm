5c7f8c3a0a89ce7bc6530f3bd2dbda7c635a1e84
szc <zhenchao.sheng@snowplus.cn>
2020-06-01 10:19:47 +0800

    u

---
+++ basic/heap_test.go
@@ -0,0 +1,31 @@
+package basic
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Heap(t *testing.T) {
+	h := new(Heap)
+	h.Init(CfInt)
+
+	h.Insert(1)
+	h.Insert(4)
+	h.Insert(2)
+	h.Insert(3)
+	fmt.Printf("top %v\n", h.Top())
+	fmt.Printf("size %v\n", h.Size())
+	fmt.Printf("data %v\n", h.Data())
+	h.Insert(5)
+	fmt.Printf("insert 5 after\ntop %v\n", h.Top())
+	fmt.Printf("size %v\n", h.Size())
+	fmt.Printf("data %v\n", h.Data())
+	h.Extract()
+	fmt.Printf("Extract after\ntop %v\n", h.Top())
+	fmt.Printf("size %v\n", h.Size())
+	fmt.Printf("data %v\n", h.Data())
+	h.Extract()
+	fmt.Printf("Extract again after\ntop %v\n", h.Top())
+	fmt.Printf("size %v\n", h.Size())
+	fmt.Printf("data %v\n", h.Data())
+}
